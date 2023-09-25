1cc844416edfeaa1f2ae37d775b8c4f59468b176
Shubham Rai <shubham56237@gmail.com>
2023-09-25 23:34:33 +0530

    added editing fucntionality

--- todo-Project.js
+++ todo-Project.js
@@ -33,3 +33,22 @@ toDoList.addEventListener("click", (e) => {
 });
 
 
+// edit button
+document.addEventListener("DOMContentLoaded", function () {
+  const editButtons = document.querySelectorAll(".edit");
+
+  // Handle Edit button clicks
+  editButtons.forEach(function (editButton) {
+    editButton.addEventListener("click", function (e) {
+      const listItem = e.target.closest("li"); // Find the closest parent <li> element
+      const textElement = listItem.querySelector(".text"); // Find the text element
+
+      // Prompt the user for a new text for the list item
+      const newText = prompt("Edit your todo:", textElement.textContent);
+
+      if (newText !== null) {
+        textElement.textContent = newText; // Update the text
+      }
+    });
+  });
+});
