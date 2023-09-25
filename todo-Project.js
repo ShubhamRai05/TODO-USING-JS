7d8329dd077e148cccbc8149f98a305948855418
Shubham Rai <shubham56237@gmail.com>
2023-09-25 23:37:29 +0530

    added comments

--- todo-Project.js
+++ todo-Project.js
@@ -1,39 +1,38 @@
+// Get references to the necessary DOM elements
 const todoForm = document.querySelector(".form-todo");
 const formInput = document.querySelector(".form-todo input[type='text']");
 const toDoList = document.querySelector(".addATodo");
 
-
-
+// Handle form submission
 todoForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const newTodoText = formInput.value;
-  formInput.value = "";
-  const newLi = document.createElement("li");
+  event.preventDefault(); // Prevent the form from submitting and refreshing the page
+  const newTodoText = formInput.value; // Get the text from the input field
+  formInput.value = ""; // Clear the input field
+  const newLi = document.createElement("li"); // Create a new list item
   const newLiInnerHtml = `<span class="text">${newTodoText}</span>
     <div class="todo-button">
         <button class="todo-btn done">Done</button>
         <button class="todo-btn remove">Remove</button>
+        <button class="todo-btn edit">Edit</button> 
+
     </div>`;
-  newLi.innerHTML = newLiInnerHtml;
-  toDoList.append(newLi);
+  newLi.innerHTML = newLiInnerHtml; // Set the inner HTML of the new list item
+  toDoList.append(newLi); // Add the new list item to the list
 });
 
-
-
+// Handle clicks on the to-do list
 toDoList.addEventListener("click", (e) => {
-  if (e.target.classList.contains("done")) {
-    const spanLi = e.target.parentNode.previousElementSibling;
-    spanLi.style.textDecoration = spanLi.style.textDecoration === "line-through" ? "none" : "line-through";
-
+  if (e.target.classList.contains("done")) { // If the clicked element has the class "done"
+    const spanLi = e.target.parentNode.previousElementSibling; // Find the previous sibling (the <span>)
+    spanLi.style.textDecoration = spanLi.style.textDecoration === "line-through" ? "none" : "line-through"; // Toggle text decoration
   }
-  if (e.target.classList.contains("remove")) {
-    const removeElement = e.target.parentNode.parentNode;
-    removeElement.remove();
+  if (e.target.classList.contains("remove")) { // If the clicked element has the class "remove"
+    const removeElement = e.target.parentNode.parentNode; // Find the parent <li> to remove
+    removeElement.remove(); // Remove the list item
   }
 });
 
-
-// edit button
+// Edit button functionality
 document.addEventListener("DOMContentLoaded", function () {
   const editButtons = document.querySelectorAll(".edit");
 
