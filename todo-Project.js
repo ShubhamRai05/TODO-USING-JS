aa0d7b62d66990db54b276c29aeb9a023394df81
Shubham Rai <shubham56237@gmail.com>
2023-09-25 23:22:00 +0530

    todo using js

---
+++ todo-Project.js
@@ -0,0 +1,28 @@
+const todoForm = document.querySelector(".form-todo");
+const formInput = document.querySelector(".form-todo input[type='text']");
+const toDoList = document.querySelector(".addATodo");
+// console.log(formInput);
+// console.log(todoForm);
+todoForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const newTodoText = formInput.value;
+  formInput.value = "";
+  const newLi = document.createElement("li");
+  const newLiInnerHtml = `<span class="text">${newTodoText}</span>
+    <div class="todo-button">
+        <button class="todo-btn done">Done</button>
+        <button class="todo-btn remove">Remove</button>
+    </div>`;
+  newLi.innerHTML = newLiInnerHtml;
+  toDoList.append(newLi);
+});
+toDoList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("done")) {
+    const spanLi = e.target.parentNode.previousElementSibling;
+    spanLi.style.textDecoration = "line-through";
+  }
+  if (e.target.classList.contains("remove")) {
+    const removeElement = e.target.parentNode.parentNode;
+    removeElement.remove();
+  }
+});
